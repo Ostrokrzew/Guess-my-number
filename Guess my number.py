0a243b85d5305a2d5d702d2c393eb518c94ff4d8
SLAWEK\Adminek <ostrokrzew@protonmail.com>
2018-09-16 15:19:51 +0200

    no message

--- Guess my number.py
+++ Guess my number.py
@@ -1,38 +1,39 @@
-# Komputr zgadiwô Twoja lëczba
+﻿# Kòmpùtr zgadiwô Twojã lëczbã
 
-import random #jimport biblioteczi
+import random #jimpòrt biblioteczi
 
-print("Wëbiérze dowolną lëczba od 0 do 100.")
-print("Komputr mdze miôł stara ja wëzdgadnąc jak nôrëchli.")
-print("Żlë lëczba podónô bez komputr je za małô, wpisze \"too low\"")
-print("Żlë lëczba podónô bez komputr je za wiôlgô, wpisze \"too high\"")
-print("\nPodô swoją lëczba")
+print("Witôj w jigrze /"Jakô to lëczba?/"")
+print("Wëbiérze dowòlną lëczbã òd 0 do 100.")
+print("Kòmpùtr mdze miôł starã jã wëzdgadnąc jak nôrëchli.")
+print("Żlë lëczba pòdónô bez kòmpùtr je za môłô, wpisze \"too low\"")
+print("Żlë lëczba pòdónô bez kòmpùtr je za wiôlgô, wpisze \"too high\"")
+print("\nPòdô swojã lëczbã")
 # nôdpisë dlô gracza
 a = 1
 b = 100 #zôkres wëzgôdczi
-chosen = int(input()) # pobranié lëczbë od gracza
+chosen = int(input()) # pòbranié lëczbë òd gracza
 choose = ""
 tries = 1
-number = random.randint(a,b) #pierszi strzél komputra
+number = random.randint(a,b) #pierszi strzél kòmpùtra
 print()
 print(number)
-while not number == chosen: #powtórziwanié strzélów bez komputr
+while not number == chosen: #pòwtórziwanié strzélów bez kòmpùtr
     choose = input()
     if choose == ("too low"):
         a = number + 1
     elif choose == ("too high"):
         b = number - 1
     else:
-        print("powtórzë wpis")
+        print("pòwtórzë wpis")
         continue
     tries += 1
-    if b < a: #warënk powtórzeniô, jeżlë chcesz oszëkac
-        print("Jes próbowôł ocëganic komputr, tej on zaczinô od zôczątku.\n")
+    if b < a: #warënk pòwtórzeniô, jeżlë chcesz oszëkac
+        print("Jes próbòwôł òcëganic kòmpùtr, zaczinô òd zôczątkù.\n")
         a = 1
         b = 100
         tries = 1
     number = random.randint(a,b) 
     print(number)
 
-print("Komputr wëzdgôdnął Twoja lëczba w", tries,"podeńscach")
-input("\nWcëskô Enter, żebë skuńczëc.")
+print("Kòmpùtr wëzdgôdnął Twojã lëczbã w", tries,"pòdeńscach")
+input("\nWcëskô Enter, żebë skùńczëc.")
---
+++ Readme.txt
@@ -0,0 +1,3 @@
+This is my second program in two modes.
+In first You're guessing computer's number, in second computer's guessing Yours.
+Biésów Smatk
