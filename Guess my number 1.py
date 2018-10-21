46e4ec2faea40a80f4213573b95b8e1246e6da3c
SLAWEK\Adminek <ostrokrzew@protonmail.com>
2018-10-21 13:02:31 +0200

    change comments on english ones

--- Guess my number 1.py
+++ Guess my number 1.py
@@ -1,34 +1,34 @@
-﻿# Biésów Smãtk
-# kòmpùter wëbiérô losowò lëczbã z zôkresë òd 1 do 100.
-# Gracz próbuje jã wëzgôdnąc, a kòmpùter gò jinformùje, czë pòdónô bez niegò lëczba je za wiôlgô, za môłô abò richtich.
+﻿#Biésów Smãtk
+#Computer choose random number from 1 to 100.
+#Player tries to guess the number and computer informs him, is his number too low, too high or good.
 
-import random #jimpòrt mòdułë
+import random
 
 print("\tWitój w jigrze \"Jakô to lëczba\"!")
 print("\nMóm na mëszlë pewną lëczbã z zôkresë òd 1 do 100.")
 print("Spróbùje jã wëzgôdnąc w jak nômiészi lëczbie próbów.\n")
 print("Môsz jejich blós 10!\n")
 
-# ùstawi zôczątkòwé wôrtnotë
+#setting start value
 number = random.randint(1, 100)
 guess = int(input("Ta lëczba to: "))
 tries = 1
 
-# pãtla zgadiwaniô
+#guessing loop
 while guess != number:
     if guess > number:
         print("Za wiôlgô...")
     else:
         print("Za môłô...")
-            
+
     guess = int(input("Ta lëczba to: "))
     tries += 1
-    if tries >= 10: #warënk przegraniô
+    if tries >= 10: #condition of failure
         break
 if tries < 10:
-    print("Jes wëzgôdł! Ta lëczba to", number)
-    print("Do dobëcégò jes brëkòwôł blós", tries, "próbów!\n")
+    print("Jes wëzgôdł! Ta lëczba to", number) #win
+    print("Do dobëcégò jes brëkòwôł blós", tries, "próbów!\n")#number of tries
 else:
-    print("Môsz przegróné, nie wëzgôdł jes w 10 próbach.")    
-  
+    print("Môsz przegróné, nie wëzgôdł jes w 10 próbach.") #failure
+
 input("\nWcëskôj Enter, żebë skùńczëc.")
--- Guess my number 2.py
+++ Guess my number 2.py
@@ -1,7 +1,7 @@
-﻿# Biésów Smãtk
-# kòmpùter zgadiwô Twojã lëczbã, wëbróną z zôkresë òd 0 do 100.
+﻿#Biésów Smãtk
+#Computer guesses player's number from 0 to 100.
 
-import random #jimpòrt mòdułë
+import random
 
 print("Witôj w jigrze \"Jakô to lëczba?\"!")
 print("Wëbiérze dowòlną lëczbã òd 0 do 100.")
@@ -9,17 +9,17 @@ print("Kòmpùter mdze miôł starã jã wëzdgadnąc jak nôrëchli.")
 print("Mô leno 10 próbów.")
 print("Żlë lëczba pòdónô bez kòmpùter je za môłô, wpisze \"too low\"")
 print("Żlë lëczba pòdónô bez kòmpùter je za wiôlgô, wpisze \"too high\"")
-print("\nPòdôj swojã lëczbã")
-# nôdpisë dlô gracza
+print("\nPòdôj swojã lëczbã") #instructions for player
+
 a = 1
-b = 100 #zôkres wëzgôdczi
-chosen = int(input()) # pòbranié lëczbë òd gracza
+b = 100 #range of values
+chosen = int(input()) #taking number from player
 choose = ""
 tries = 1
-number = random.randint(a,b) #pierszi strzél kòmpùtra
+number = random.randint(a,b) #first computer's try
 print()
 print(number)
-while not number == chosen: #pòwtórziwanié strzélów bez kòmpùter
+while not number == chosen: #next tries
     choose = input()
     if choose == ("too low"):
         a = number + 1
@@ -29,17 +29,17 @@ while not number == chosen: #pòwtórziwanié strzélów bez kòmpùter
         print("pòwtórzë wpis")
         continue
     tries += 1
-    if b < a: #warënk pòwtórzeniô, jeżlë chcesz oszëkac
+    if b < a: #condition of restart if player tries to cheat
         print("Jes próbòwôł òcëganic kòmpùter, zaczinô òn òd zôczątkù.\n")
         a = 1
         b = 100
         tries = 1
-    number = random.randint(a,b) 
+    number = random.randint(a,b)
     print(number)
-    if tries >= 10: #warënk przegraniô
+    if tries >= 10: #failure's condition
         break
 if tries < 10:
-    print("Kòmpùter wëzdgôdnął Twojã lëczbã w", tries,"pòdeńscach")
+    print("Kòmpùter wëzdgôdnął Twojã lëczbã w", tries,"pòdeńscach") #computer wins
 else:
-    print("Kòmpùter nie wëzgôdł w 10 próbach. Jes dobëł")
+    print("Kòmpùter nie wëzgôdł w 10 próbach. Jes dobëł") #player wins
 input("\nWcëskôj Enter, żebë skùńczëc.")
